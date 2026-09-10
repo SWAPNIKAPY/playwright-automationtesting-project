b131fc22958bf6dccfdec0c712a219f3ba279109
SWAPNIKAPY <swapnika.pulipaty@gmail.com>
2026-09-10 12:44:11 +0530

    Fix autocomplete test

--- tests/autocomplete.spec.js
+++ tests/autocomplete.spec.js
@@ -3,10 +3,10 @@ import { test, expect } from '@playwright/test';
 test('autocomplete field displays matching suggestions', async ({ page }) => {
   await page.goto('/AutoComplete.html');
 
-  autocomplete = page.locator('input.ui-autocomplete-input');
+  let autocomplete = page.locator('input.ui-autocomplete-input');
 
   await autocomplete.fill('Ind');
 
-  suggestions = page.locator('.ui-autocomplete li');
+  let suggestions = page.locator('.ui-autocomplete li');
   await expect(suggestions.first()).toBeVisible();
 });
